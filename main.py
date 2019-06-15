fa6fab81f9b66cfb9c9f30726a70ee9808f4a375
liuyuan111 <2386023698@qq.com>
2019-06-15 21:44:59 +0800

    6/15

--- main.py
+++
@@ -1,9 +0,0 @@
-# -*- coding:utf-8 -*-
-__author__ = 'liuyuan'
-
-from scrapy.cmdline import execute
-import sys
-import os
-
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
-execute(["scrapy", "crawl", "jobbole"])
